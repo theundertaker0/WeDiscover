1c8759fbcb7638fbdefde23756f597c14132589f
lbeatriz <lbeatriz@itsptogreso.edu.mx>
2020-04-01 01:26:57 -0600

    Cambio en logo de slashscreen y colores de menú lateral (aun no finales)

--- app/src/main/java/com/kubo79/android/wediscover/Main2Activity.kt
+++ app/src/main/java/com/kubo79/android/wediscover/Main2Activity.kt
@@ -1,17 +1,19 @@
 package com.kubo79.android.wediscover
 
 
+
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import android.view.Menu
+
 
 class Main2Activity : AppCompatActivity() {
 
@@ -27,6 +29,7 @@ class Main2Activity : AppCompatActivity() {
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
+        navView.setBackgroundColor(resources.getColor(R.color.darkGrey))
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
